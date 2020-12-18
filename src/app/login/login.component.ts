8a814fd55e892a05704f9a9df577337c2f0da798
PrinceOsa <chidiosakwe@outlook.com>
2020-12-18 16:43:25 -0500

    changes v5

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -45,7 +45,7 @@ export class LoginComponent implements OnInit {
             if(res && res.data && res.data.success) {
                 const token = res.data.token;
                 localStorage.setItem('jwt',token);
-                //location.assign('/Dashboard');
+                location.assign('/Dashboard');
                 history.pushState({pageID: 'Dashboard'}, 'Dashboard', '/Dashboard');
               }
         });
